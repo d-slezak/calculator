8cfdcbada5ec009c446d9b4bb544e8bf97436853
d-slezak <Slezak.Dan@outlook.com>
2022-05-31 19:22:18 -0400

    add display function to numButtons

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const inputDisplay = document.querySelector('#input');
+
 function add(x, y){
     return x + y;
 }
@@ -32,3 +34,15 @@ function operate(operator, x , y){
             console.log(operator)
     }
 }
+
+function displayNum(x)
+{
+    if(inputDisplay.textContent == 0)
+    {
+        inputDisplay.textContent = "";
+    }
+    inputDisplay.textContent += x;
+}
+
+const numButtons = document.querySelectorAll('.num');
+numButtons.forEach(btn => btn.addEventListener('click' , () => {displayNum(btn.textContent)}))
